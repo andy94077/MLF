7aafc974c2975b258979c88b58517abd8e943f27
andy94077 <andy94077@gmail.com>
2019-01-12 17:43:32 +0800

    [hw3]revise dE_norm

--- HW3/18-20.py
+++ HW3/18-20.py
@@ -7,26 +7,32 @@ def theta(s):
 	'''@retrun: double'''
 	if s < -20:
 		return 0
-	else if s > 20:
+	elif s > 20:
 		return 1
 	else:
 		return 1 / (1 + exp(-s))
 
-def dE_norm(w, data, y):
+def dE_norm(w, data, y,sgd):
 	'''@return: normalized dE ndarray'''
-	dE=sum([theta(-y[i] * w.dot(data[i])) * (-y[i] * data[i]) for i in sample(range(data.shape[0]),50)]) / data.shape[0]
-	return np.linalg.norm(dE)
-	
-def PLA(data, y, test, eta, update_time):
-	'''@return: int. the number of errors after PLA'''
+	if sgd:
+		dE=theta(-y[dE_norm.n] * w.dot(data[dE_norm.n])) * (-y[dE_norm.n] * data[dE_norm.n])
+		dE_norm.n = (dE_norm.n + 1) % 50
+	else:
+		dE=sum([theta(-y[i] * w.dot(data[i])) * (-y[i] * data[i]) for i in sample(range(data.shape[0]),50)]) / 50
+	return dE/np.linalg.norm(dE)
+dE_norm.n=0
+
+def PLA(data, y, test, eta, sgd=False, update_time=2000):
+	'''@return: int. The number of errors after PLA'''
 	w = np.zeros(data.shape[1])
 	for _ in range(update_time):
 		print('%d ' %_ if _%10==0 else '',end='')
 		for i, x in enumerate(data):
 			if sign(w.dot(x)) != y[i]:
-				w -= eta * dE_norm(w, data, y)
+				w -= eta * dE_norm(w, data, y,sgd)
 		print('\n' if _%100==0 else '',end='')
 	return sum([sign(w.dot(line[:-1]))!=line[-1] for line in test])
-	
+
+
 train, test = read_data('hw3_train.dat'), read_data('hw3_test.dat')
-print(PLA(train[:,:-1], train[:, -1], test, 0.001, 2000))
+print('\n',PLA(train[:,:-1], train[:, -1], test, 0.001)/test.shape[0])
